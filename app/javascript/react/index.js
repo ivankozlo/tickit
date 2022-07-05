0b017e320fba211448652ac300cfb50a0cdf7c7b
ivankozlo <ivan.olegovich.kozlov@gmail.com>
2022-07-06 03:07:35 +1000

    Added trello board

--- app/javascript/react/index.js
+++ app/javascript/react/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-const App = () => {
-  return <div>I am the React component, Hola</div>
-}
+import App from './App'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
 
-root.render(<App />)
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(
+    <App />
+  );
+});
